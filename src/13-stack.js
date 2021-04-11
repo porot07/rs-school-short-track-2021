e255f895a876f7349d18589ff421449e1950768f
porot07 <ciscomalware@github.com>
2021-04-11 23:50:30 +0500

    feat: add solution stack

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,20 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.array = [];
+  }
+
+  push(el) {
+    return this.array.push(el);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.array.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.array[this.array.length - 1];
   }
 }
 
