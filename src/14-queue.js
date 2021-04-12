1cc0a5cd40fc6c9107fd44bfcfe3b600e85dbff3
porot07 <ciscomalware@github.com>
2021-04-12 07:34:03 +0500

    feat: add solution queue

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,33 @@
  */
 
 class Queue {
+  constructor() {
+    this.start = null;
+    this.finish = null;
+  }
+
   get size() {
     throw new Error('Not implemented');
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  enqueue(el) {
+    const node = new ListNode(el);
+    node.value = el;
+    if (this.finish) {
+      this.finish.next = node;
+      this.finish = node;
+    } else {
+      this.finish = node;
+      this.start = node;
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    if (!this.start) return null;
+    if (this.start === this.finish) this.finish = null;
+    const deleted = this.start;
+    this.start = this.start.next;
+    return deleted.value;
   }
 }
 
